f65c44e34d191c1bbbc01983630d02b6fd50f487
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-10-01 12:31:16 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import SingleDataView from "./components/SIngleDataView/SingleDataView";
+import Filters from "./components/Filters/Filters";
+import List from "./components/List/List";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="container">
+			<Filters />
+			<List />
+			<Routes>
+				<Route path="/singleUser/:id" element={<SingleDataView />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
