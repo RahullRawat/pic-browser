73963e8ae25c9812e7e05b64bea5df368bfb2f33
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-10-01 12:33:01 +0530

    data-context

---
+++ src/context/dataContext.js
@@ -0,0 +1,33 @@
+import { useContext, createContext, useEffect, useReducer } from "react";
+import { dataReducer } from "../reducer/dataReducer";
+
+const DataStoreContext = createContext(null);
+
+const DataStoreProvider = ({ children }) => {
+	const [dataStore, dataDispatch] = useReducer(dataReducer, {
+		data: [],
+		sliderRange: 0,
+		sortBy: null,
+	});
+
+	const fetchUsers = async () => {
+		await fetch("http://www.reddit.com/r/pics/.json?jsonp=")
+			.then((res) => res.json())
+			.then((data) =>
+				dataDispatch({ type: "INITIAL_DATA", payload: data.data.children })
+			);
+	};
+
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	return (
+		<DataStoreContext.Provider value={{ dataStore, dataDispatch }}>
+			{children}
+		</DataStoreContext.Provider>
+	);
+};
+
+const useData = () => useContext(DataStoreContext);
+export { useData, DataStoreProvider };
